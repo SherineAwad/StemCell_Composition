2a1c5cf53f25ab783ca965cf26ef9a22020c8517
SherineAwad <sherine.awad@gmail.com>
2021-11-23 15:38:52 +0000

    Update pipelines, scripts, and reference pipelines

---
+++ pipeline/src/1.Create_region_list_bam-readcount.R
@@ -0,0 +1,32 @@
+# Extract from vcf the chr, start column, and add an end column
+# Filter VCF to only the variants that are different across all donors
+# Input: VCF of non-identical SNPs for all donors
+# Output: for input SNPs, just chr, pos and pos (start and end are the same, it's 1bp)
+
+library(vcfR)
+library(data.table)
+library(tidyverse)
+options(stringsAsFactors = FALSE)
+
+args <- commandArgs(trailingOnly = TRUE)
+vcf_path = args[1] #"input_for_create_region_list_bam-readcount/test_vcf.vcf"
+output_path = args[2] # "output_for_create_region_list_bam-readcount/test_vcf.txt"
+  
+
+# Function to convert gt vcf to minor allele dosages
+
+message("...Reading VCF...")
+
+
+vcf = vcfR::read.vcfR(vcf_path)
+
+message("...Getting right columns...")
+
+subset = cbind(vcf@fix[,"CHROM"],vcf@fix[,"POS"])
+subset=as.data.frame(subset)
+print(head(subset))
+subset$V2 = as.numeric(subset$V2)
+subset$V3 = subset$V2
+subset = as.data.table(subset)
+fwrite(subset,file = output_path, 
+       sep = "\t", col.names = FALSE)
